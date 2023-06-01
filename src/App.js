09fe458de8b1bace874cf5fadfead3c2ff8f3e88
Chris-Frey <chris.frey@live.com>
2023-06-01 08:48:17 -0700

    clean refactor - do not add to pull requests, this is a personal endeavor

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
-import { click } from '@testing-library/user-event/dist/click'
+import { flushSync } from 'react-dom'
 
 const App = () => {
-  const [hasAWinner, setAWinner] = useState(false)
   const [squares, setSquares] = useState(Array(9).fill(null))
-  const [theWinner, setTheWinner] = useState("")
-
-  // sets newSquaresArray to copy of squares, sets selected targetIndex to an 'x', then sets squares to the new array values.
-  const [numberOfClicks, setNumberOfClicks] = useState(0)
-    // player array states
-  const [xPlayer, setXplayer] = useState([])
-  const [oPlayer, setOplayer] = useState([])
-  // const [choiceOne, setChoiceOne] = useState([])
-
-  // const playerChoices = [empireIcon, rebelIcon, jediIcon, niteIcon ]
-
-  const empireIcon = <img className="empIcon" src="https://i.stack.imgur.com/hgc0o.png"/>
-  
-  const rebelIcon = <img className="rebIcon" src="https://upload.wikimedia.org/wikipedia/commons/2/20/Emblem_of_the_Rebel_Alliance.svg"/>
-
-  // const jediIcon = <img className="rebIcon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/star-wars-episode-7/9/9c/Jedi-Order-Insignia.png?width=2240"/>
-
-  // const niteIcon = <img className="rebIcon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/star-wars-episode-7/8/80/Nite_owls.png?width=2240"/>
-
-
-  const lines = [
+  const [counter, setCounter] = useState(1)
+  const [player1, setPlayer1] = useState([])
+  const [player2, setPlayer2] = useState([])
+  const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -36,114 +18,73 @@ const App = () => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-
-  // choose players
-  const playerChoice = (indexNum) => {
-
-  }
-  // Game loop
-  const playerClicks = (targetIndex) => {
-    updateClicks()
-    updateArray(targetIndex)
-  }
-
-    // check if player array includes an array element.
-  const winCheck = (array, string) => {
-    
-    const containsAllElements = 
-    lines.some(line => line.every(element => array.includes(element)));
+  // const empireIcon = <img className="empIcon" src="https://i.stack.imgur.com/hgc0o.png"/>
   
-    if(containsAllElements) {
-      setTheWinner(string)
-      setAWinner(true)
+  // const rebelIcon = <img className="rebIcon" src="https://upload.wikimedia.org/wikipedia/commons/2/20/Emblem_of_the_Rebel_Alliance.svg"/>
+
+  // const jediIcon = <img className="rebIcon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/star-wars-episode-7/9/9c/Jedi-Order-Insignia.png?width=2240"/>
+
+  // const niteIcon = <img className="rebIcon" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/star-wars-episode-7/8/80/Nite_owls.png?width=2240"/>
+console.log(`player1: ${player1}`)
+console.log(`player2: ${player2}`)
+
+  // make square clickable
+  // make 9 squares on the page 
+  // put an X in the square 
+  // switch between X and O 
+  // don't allow squares to be clicked twice
+  
+  // adds player's choices to their individual array 
+    const playerSquares = (index) => {
+      if(counter % 2 === 0) {
+        flushSync(() => {
+          setPlayer2([...player2, index])
+        })
+      } else {
+        flushSync(() => {
+          setPlayer1([...player1, index])
+        })
+      }
     }
-    if(numberOfClicks >= 8) {
-      setAWinner(true) 
-      setTheWinner("It's a tie, nobody")
+
+// decides which player's turn it is based on odd or even
+    const playerPick = (index) => {
+      playerSquares(index)
+      winnerCheck(player2)
+      if(counter % 2 === 0) {
+        setCounter(counter + 1)
+        return "O"
+      } else {
+        setCounter(counter + 1)
+        return "X"
+      }
     }
-    // check after each turn
-    // go to win condition if player wins
-  }
-
-
-  // const winCheck = () => {
-  //   const containsAllElements = 
-  //   lines.some(line => line.every(element => xPlayer.includes(element)));
-  //   console.log(xPlayer);
-  //   const ocontainsAllElements = 
-  //   lines.some(line => line.every(element => oPlayer.includes(element)));
-  //   console.log(oPlayer);
-  //   if(containsAllElements) {
-  //     alert(" X player wins!")
-   
-  //   } else if (ocontainsAllElements){
-  //     alert("Y player wins")
-  //   }
-  //   if(numberOfClicks >= 8) {
-  //     alert("It's a tie!")
-  //   }
-  // }
-
-  // updates the number of clicks, and checks conditions
-  const updateClicks = () => {
-    let clickNumber = numberOfClicks + 1
-    setNumberOfClicks(clickNumber)
-  }
-
-
-//  checks wither an X or O is played
-  const updateArray = (targetIndex) => {
-    let newSquaresArray = [...squares]
-    if(numberOfClicks % 2 === 0){
-      newSquaresArray[targetIndex] = empireIcon
-      setSquares(newSquaresArray)
-      let newPlayerXArray = [...xPlayer, targetIndex]
-      setXplayer(newPlayerXArray)
-      winCheck(newPlayerXArray, "The Empire")
-      console.log(xPlayer)} 
+ 
+    // check if player array contains a winning condition
+    const winnerCheck = (player) => {
+      let partOf = (value) => player.includes(value)
     
-    else {newSquaresArray[targetIndex] = rebelIcon
-      setSquares(newSquaresArray)
-      let newPlayerOArray = [...oPlayer, targetIndex]
-      setOplayer(newPlayerOArray)
-      winCheck(newPlayerOArray, "The Rebel Alliance")
-      console.log(oPlayer)} 
-  }
-
-  const refreshPage = () => {window.location.reload()}
-
-  const turnIcon = () => {return (!hasAWinner && numberOfClicks % 2 === 1) ? rebelIcon : empireIcon}
-
-
-  const turnText = () => {return (!hasAWinner && numberOfClicks % 2 === 1) ? "The Rebel Alliance's Turn" : "The Empire's Turn"}
-
+        for(let i = 0; i < player.length; i++) {
+          if(winConditions[i].every(partOf)) {
+            return alert("winner!") 
+          }
+        }
+      }
+        
+ 
   return (
     <>
-      <h1><span className="title">Tic Tac Toe</span></h1>
-      <div className="gameBoard">
-
-        {/* maps over 'squares' state to create board */}
-        {squares.map((value, index) => {
-          return <Square 
-          key={index}
-          index={index}
-          value={value}
-          // passes function through to component for use
-          playerClicks={playerClicks}
+      <h1>Tic Tac Toe</h1>
+        <div className='gameBoard'>
+          {squares.map((value, index) => {
+            return <Square 
+              value={value} 
+              index={index}
+              key={index}
+              playerPick={playerPick}
           />
-        })}
-        
-        {hasAWinner &&   <>
-          <div className='winMessage'>
-            <div className = "winText"> {theWinner} has won the game!</div>
-            <button className="button" onClick={refreshPage}>Restart?</button>
-  
-          </div>
-          </>}
-          {numberOfClicks > 0 && !hasAWinner ? turnIcon() : null}
-          {numberOfClicks > 0 && !hasAWinner ? turnText() : null}
-
-      </div>
+          })}
+        </div>
     </>
   )
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,22 +1,16 @@
 import React, { useState } from 'react'
 
-const Square = (props) => {
 
-  // status of whether the square has been clicked
-  const [clickedStatus, setClickedStatus] = useState(false)
-  // checks if the square has been clicked (true) or not. If clicked, short-circuites and does not allow a state change. otherwise changes state, and sets status to true
-  const handleClick = () => {
-    if (clickedStatus) {
-      return
-    } else {
-      setClickedStatus(true)
-      props.playerClicks(props.index)
-    }
-  }
+
+const Square = (props) => {
+  const [squareVal, setSquareVal] = useState(null)
 
   return (
-    // calls handleClick when square is clicked
-    <div className="square" onClick={handleClick}>{props.value}</div>
+    <div className="square" onClick={() => {
+      squareVal === null ? setSquareVal(props.playerPick(props.index)) : alert("Choose Another Square")
+    }}
+    >{squareVal}</div>
   )
 }
 export default Square
+
